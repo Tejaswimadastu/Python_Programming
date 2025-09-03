cef0141af04033addaced9e5a3c7d3235fd4af4f
Tejaswimadastu <163648336+Tejaswimadastu@users.noreply.github.com>
2025-09-03 15:23:49 +0530

    Delete leap.py

--- leap.py
+++
@@ -1,8 +0,0 @@
-def leap(n):
-    if (n%100!=0 and n%4==0) or (n%100==0 and n%4==0 and n%400==0):
-        print(f"The leap year is {n}")
-    else:
-        print("Not a leap year")
-leapy=int(input("Enter year "))
-leap(leapy)
-
