95e57cb2ad71e360688540e7bac8d906d7e5f04c
Tejaswimadastu <163648336+Tejaswimadastu@users.noreply.github.com>
2025-09-03 15:24:47 +0530

    Delete prog6.py

--- prog6.py
+++
@@ -1,8 +0,0 @@
-#simple intrest
-p=int(input("Enter principle amount "))
-r=int(input("Rate of Interest "))
-t=int(input("Total number of months "))
-si=(p*r*t)/100
-print(si);
-total=si+p
-print(total)
