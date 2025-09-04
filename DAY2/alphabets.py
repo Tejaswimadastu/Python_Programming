5bcba2c38ddac3e3d11de3f8332f41cb82569e8e
Tejaswi <tejaswimadastu10@gmail.com>
2025-09-04 10:52:05 +0530

    Day2 python programming

---
+++ DAY2/alphabets.py
@@ -0,0 +1,6 @@
+import string
+def alphabets():
+    print(string.ascii_lowercase)  
+    print(string.ascii_uppercase)  
+
+alphabets()
---
+++ DAY2/n_natural.py
@@ -0,0 +1,7 @@
+def natural(n):
+    i=1
+    while(i<=n):
+        print(i)
+        i=i+1
+x=int(input("Enter a number to print natural numbers "))
+print(natural(x))
